d3137652e8a0d46ec960d03de04785fd8e26b68e
adityagupta0123 <aditya.gupta3182003@gmail.com>
2024-08-08 22:53:01 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TrafficLightSystem from './TrafficLightSystem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Traffic Light System</h1>
+      <TrafficLightSystem />
+    </div>
+  );
+}
+
+export default App;
